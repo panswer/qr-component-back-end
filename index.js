39e75ad259a3ed30bc325c5a4abcd5ee4a88eacc
Ricardo Mejias <panswer.rjmm@gmail.com>
2022-07-03 18:33:07 -0400

    update: join back-end and front-end

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+
+const staticFolder = path.resolve(__dirname, './wwww/');
+
+if (!fs.existsSync(staticFolder)) {
+    console.log("www folder don't found");
+    process.exit(1);
+}
+app.use(express.static(staticFolder));
+
+app.get('/*', (_req, res) => {
+    try {
+        const indexHTML = path.resolve(staticFolder, 'index.html');
+
+        if (!fs.existsSync(indexHTML)) {
+            res.status(404).send('Page Not Found');
+        } else {
+            res.sendFile(indexHTML);
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Internal error');
+    }
+});
+
+const port = process.env.PORT || 3001;
+
+app.listen(
+    port,
+    err => err ?
+        console.log(err) :
+        console.log('Server on')
+);
